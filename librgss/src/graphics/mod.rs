42a96c9d7b6d1151cc6376a1456f38750796930b
Speak2Erase <lily@nowaffles.com>
2024-02-29 13:26:59 -0800

    Initialze wgpu

--- librgss/src/graphics/mod.rs
+++ librgss/src/graphics/mod.rs
@@ -15,16 +15,86 @@
 // You should have received a copy of the GNU General Public License
 // along with rsgss.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::sync::Arc;
+
+use winit::window::Window;
+
+use color_eyre::eyre::OptionExt;
+
 use crate::event_loop::EventLoop;
 
 pub struct Graphics {
-    window: winit::window::Window,
+    window: Arc<Window>,
+    pub(crate) graphics_state: GraphicsState,
+}
+
+pub(crate) struct GraphicsState {
+    pub(crate) instance: wgpu::Instance,
+    pub(crate) surface: wgpu::Surface<'static>,
+    pub(crate) adapter: wgpu::Adapter,
+    pub(crate) device: wgpu::Device,
+    pub(crate) queue: wgpu::Queue,
 }
 
 impl Graphics {
-    pub fn new(event_loop: &EventLoop) -> color_eyre::Result<Self> {
-        let window = winit::window::Window::new(&event_loop.event_loop)?;
+    pub async fn new(event_loop: &EventLoop) -> color_eyre::Result<Self> {
+        let window = winit::window::WindowBuilder::new()
+            .with_inner_size(winit::dpi::PhysicalSize::new(640, 480))
+            .build(&event_loop.event_loop)
+            .map(Arc::new)?;
+        let graphics_state = GraphicsState::new(window.clone()).await?;
 
-        Ok(Self { window })
+        Ok(Self {
+            window,
+            graphics_state,
+        })
+    }
+}
+
+impl GraphicsState {
+    async fn new(window: Arc<Window>) -> color_eyre::Result<Self> {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            flags: wgpu::InstanceFlags::from_build_config(),
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window)?;
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptionsBase {
+                power_preference: wgpu::util::power_preference_from_env()
+                    .unwrap_or(wgpu::PowerPreference::HighPerformance),
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .ok_or_eyre("failed to find suitable adapter")?;
+
+        let surface_config = surface
+            .get_default_config(&adapter, 640, 480)
+            .ok_or_eyre("surface not supported")?;
+
+        // TODO optimizations based on certain features/limits
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: Some("librgss device"),
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await?;
+
+        surface.configure(&device, &surface_config);
+
+        Ok(Self {
+            instance,
+            surface,
+            adapter,
+            device,
+            queue,
+        })
     }
 }
--- sapphire/src/main.rs
+++ sapphire/src/main.rs
@@ -1,10 +1,12 @@
+use pollster::FutureExt;
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
     let (event_loop, events) = librgss::EventLoop::new()?;
 
     let audio = librgss::Audio::new()?;
-    let graphics = librgss::Graphics::new(&event_loop)?;
+    let graphics = librgss::Graphics::new(&event_loop).block_on()?;
 
     #[cfg(feature = "magnus")]
     let bindings = unsafe { rsgss_binding_magnus::Bindings::new() };
